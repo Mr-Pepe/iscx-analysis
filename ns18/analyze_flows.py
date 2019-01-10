f24d1e38002ae2dcee1e22323176e5ea40e85db4
Felipe <felipepeter@web.de>
2019-01-10 16:30:23 +0800

    Create table with results

--- ns18/analyze_flows.py
+++ ns18/analyze_flows.py
@@ -1,12 +1,15 @@
 import pickle
 import os
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
 
+matplotlib.use('Qt5Agg')
 # Set console width
 desired_width = 320
 np.set_printoptions(linewidth=desired_width)
 
-p_files_path = "../datasets/flows"
+p_files_path = "../datasets/all_flows_with_packets"
 
 # Order is important because 'tor' is included in 'torrent' and tor filenames also include other app names
 app_names = ['torrent',
@@ -31,14 +34,15 @@ flows_by_app = {key: {} for key in app_names}
 labels = []
 i_file = 0
 
+use_vpn = True
+
 for dirpath, dirnames, filenames in os.walk(p_files_path):
 
     for filename in filenames:
         path = os.path.join(dirpath, filename)
 
         # Look at non-vpn and non-tor files
-        # if filename.find('vpn') == -
-        if True:
+        if use_vpn or filename.find('vpn') == -1:
             found = 0
             app_name = ''
             # Organize by application
@@ -60,15 +64,16 @@ for dirpath, dirnames, filenames in os.walk(p_files_path):
                     # Discard IP addresses
                     flow = flow[2:]
 
-                    if not flow in flows_by_app[app_name]:
+                    if flow not in flows_by_app[app_name]:
                         flows_by_app[app_name][flow] = 0
 
                     flows_by_app[app_name][flow] += n_packets
 
                 i_file += 1
 
-## Calculate the flow overlap between apps
-overlap = np.zeros((len(app_names), len(app_names)),dtype='int')
+# Calculate the flow overlap between apps
+n_overlapping_flows = np.zeros((len(app_names), len(app_names)), dtype='int')
+n_ambiguous_packets = np.zeros((len(app_names), len(app_names)), dtype='int')
 # Which flows in an app are also used in another app
 overlapping_flows_by_app = {key: {} for key in app_names}
 
@@ -76,13 +81,13 @@ for i_app1, app1 in enumerate(app_names):
     for i_app2, app2 in enumerate(app_names):
         if i_app1 != i_app2:
             overlapping_flows = set(flows_by_app[app1].keys()) & set(flows_by_app[app2].keys())
-            overlap[i_app1, i_app2] = len(overlapping_flows)
+            n_overlapping_flows[i_app1, i_app2] = len(overlapping_flows)
 
             for flow in overlapping_flows:
                 overlapping_flows_by_app[app1][flow] = flows_by_app[app1][flow]
 
 # Percentage of flows in an app also used in another app
-flow_overlap_by_app = {key: 0 for key in app_names}
+unique_flows_by_app = {key: 0 for key in app_names}
 # Percentage of packets recorded for an app that can be clearly associated with it
 unambiguous_packets_by_app = {key: 0 for key in app_names}
 
@@ -98,25 +103,40 @@ for app in app_names:
     n_unique_flows_this_app     = n_flows_this_app - n_non_unique_flows_this_app
     flow_overlap_this_app       = n_unique_flows_this_app / n_flows_this_app
 
-    # print("%.2f %% of the %s flows are unique, which is %d of %d" % (flow_overlap_this_app * 100, app, n_unique_flows_this_app, n_flows_this_app))
-
     # Calculate percentage of unambiguous packets
     n_packets_this_app              = sum(list(flows_by_app[app].values()))
     n_ambiguous_packets_this_app    = sum(list(overlapping_flows_by_app[app].values()))
     n_unambiguous_packets_this_app  = n_packets_this_app - n_ambiguous_packets_this_app
     unambiguous_packets_this_app    = n_unambiguous_packets_this_app / n_packets_this_app
 
-    print("%.2f %% of the %s packets can be associated directly with it, which is %d of %d" % (unambiguous_packets_this_app * 100, app, n_unambiguous_packets_this_app,  n_packets_this_app))
-
     unambiguous_packets_by_app[app] = unambiguous_packets_this_app
-    flow_overlap_by_app[app]        = flow_overlap_this_app
+    unique_flows_by_app[app]        = flow_overlap_this_app
 
     n_flows_total += n_flows_this_app
     n_packets_total += n_packets_this_app
     n_unique_flows_total += n_unique_flows_this_app
     n_unambiguous_packets_total += n_unambiguous_packets_this_app
 
-
-
-print("%f%% of all flows can be associated with a specific application" % (n_unique_flows_total / n_flows_total))
+print("%f%% of all all_flows_with_packets can be associated with a specific application" % (n_unique_flows_total / n_flows_total))
 print("%f%% of all packets can be associated with a specific application" % (n_unambiguous_packets_total / n_packets_total))
+
+table_content = np.empty((18, 5), dtype='object')
+col_labels = ("Application", "Number of all_flows_with_packets", "Number of packets", "Unique all_flows_with_packets", "Unambiguous packets")
+table_content[:, 0]      = np.array([app_names + ["Total"]])[0]
+table_content[:-1, 1]    = np.array([len(flows) for app, flows in flows_by_app.items()])
+table_content[-1, 1]     = table_content[:-1, 1].sum()
+table_content[:-1, 2]    = np.array([sum(list(flows.values())) for flows in list(flows_by_app.values())])
+table_content[-1, 2]     = table_content[:-1, 2].sum()
+table_content[:-1, 3]    = np.array(list(unique_flows_by_app.values())).round(decimals=2)
+table_content[-1, 3]     = np.round(n_unique_flows_total / n_flows_total, decimals=2)
+table_content[:-1, 4]    = np.array(list(unambiguous_packets_by_app.values())).round(decimals=2)
+table_content[-1, 4]     = np.round(n_unambiguous_packets_total / n_packets_total, decimals=2)
+
+fig = plt.figure()
+ax = plt.gca()
+the_table = ax.table(cellText=table_content, colLabels=col_labels, loc='center')
+table_props = the_table.properties
+the_table.auto_set_font_size(False)
+the_table.set_fontsize(16)
+
+plt.show()
